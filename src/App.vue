381e1787f0342c8964118f3924a71aa68c140d76
flaviocampos <flaviorcampos15@gmail.com>
2024-12-10 21:42:01 -0300

    Aula 337

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Calculator from './main/Calculator.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Calculator }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  color: #fff;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 71%, rgba(0, 212, 255, 1) 100%);
 }
 </style>
---
+++ src/components/Display.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div class="display">
+        0
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
